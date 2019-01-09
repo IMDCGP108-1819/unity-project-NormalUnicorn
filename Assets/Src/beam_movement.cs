ae5fcdccc249c59f8cd454f3347ce87cdcae112b
Jordan <S193387@UOS.AC.UK>
2019-01-09 13:25:08 +0000

    added firing beam

---
+++ Assets/Src/beam_movement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class beam_movement : MonoBehaviour {
+    public float movement_speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(0f, movement_speed*Time.deltaTime);
+    }
+}
--- Assets/Src/user_input.cs
+++ Assets/Src/user_input.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class user_input : MonoBehaviour {
-
+	public GameObject fired_beam;
 	// Use this for initialization
 	void Start () {
 
@@ -11,6 +11,8 @@ public class user_input : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-
+		if (Input.GetKeyDown("space")) {
+			Instantiate(fired_beam, transform.position, Quaternion.identity);
+		}
 	}
 }
