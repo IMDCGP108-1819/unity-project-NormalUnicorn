ba0dfe8567b0303d1048cef8d7a3bc79d9f3479e
Jordan <JordanChell@vivaldi.net>
2019-01-11 08:10:13 +0000

    added background art

--- Assets/Src/beam_movement.cs
+++ Assets/Src/beam_movement.cs
@@ -21,28 +21,29 @@ public class beam_movement : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        //moves the game object up along y axis
         transform.position += new Vector3(0f, movement_speed*Time.deltaTime);
     }
 
     void OnTriggerEnter2D(Collider2D other) {
+          // when the beam hits an object, both objects are set inactive and score is increased based on the item hit
           if (other.gameObject.tag == "goal1") {
                 other.gameObject.SetActive(false);
                 this.gameObject.SetActive(false);
                 score.GetComponent<scoring>().score += 1;
+
           } else if (other.gameObject.tag == "goal2") {
                 other.gameObject.SetActive(false);
                 this.gameObject.SetActive(false);
                 score.GetComponent<scoring>().score += 3;
          } else if (other.gameObject.tag == "goal3") {
                other.gameObject.SetActive(false);
-               other.gameObject.SetActive(false);
+               this.gameObject.SetActive(false);
                score.GetComponent<scoring>().score += 5;
+         // if the user misses and goes out of bounds they lose a life
          } else if (other.gameObject.tag == "boundry") {
                this.gameObject.SetActive(false);
                lives.GetComponent<lives>().lives_counter -= 1;
-               if (lives.GetComponent<lives>().lives_counter == 0) {
-                     end_pannel.SetActive(true);
-               }
          }
    }
 }
--- Assets/Src/lives.cs
+++ Assets/Src/lives.cs
@@ -8,6 +8,10 @@ public class lives : MonoBehaviour
       public int lives_counter;
       public Text diplayed_lives;
       public GameObject final_panel;
+      public GameObject Spawn1;
+      public GameObject Spawn2;
+      public GameObject SpawnLaser;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +24,9 @@ public class lives : MonoBehaviour
           diplayed_lives.text = "Lives:" + lives_counter;
           if (lives_counter == 0) {
                 final_panel.SetActive(true);
+                Spawn1.SetActive(false);
+                Spawn2.SetActive(false);
+                SpawnLaser.SetActive(false);
           }
     }
 }
